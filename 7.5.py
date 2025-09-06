812bf39d128be9c83f440ce6c53902091f6e23e8
Cristiano <cristianoferrara.2006@gmail.com>
2025-09-06 17:11:54 +0200

    Dealer turn optimized

--- 7.5.py
+++ 7.5.py
@@ -14,7 +14,12 @@ def draw_card():
     card_suit = number_drawn // 10           #gets the integer part of the divison to determine the suit       
     card_value = number_drawn % 10           #determinates the value of the card
     real_value = card_value + 1            #gives the real value of the card(lists starts with 0)
-    print("Your card:", value[card_value], "of", cards[card_suit])       #print the name of the card
+    
+    if dealer_turn == False:              #checks if it's the turn of the player or the dealer
+        print("Your card:", value[card_value], "of", cards[card_suit])       #print the name of the card
+    else:
+        print("Dealer card:", value[card_value], "of", cards[card_suit])      #print the name of the card
+
     return real_value, number_drawn
 
 def manage_jolly(isJolly, totPoints):
@@ -24,11 +29,18 @@ def manage_jolly(isJolly, totPoints):
         elif totPoints % 1 != 0 or totPoints == 7:          #checks if your points haves an half
             jolly_total_points = 7.5                        #gives you the maximum of points because of the jolly
         sleep(0.5)
+
     if isJolly == 0 or totPoints > 7.5:                     #checks if the jolly was not drawn and gives a print of total points
-        print("You have", totPoints)
+        if dealer_turn == False:
+            print("You have", totPoints)
+        else:
+            print("Dealer has", totPoints)
         return totPoints
     elif isJolly == 7 and totPoints <= 7.5:                 #checks if the jolly wasa drawn and gives the total points with the jolly
-        print("You have", jolly_total_points)
+        if dealer_turn == False:
+            print("You have", jolly_total_points)
+        else:
+            print("Dealer has", jolly_total_points)
         return jolly_total_points
     
 def manage_points_card(isJolly, cardPoints, totPoints):
@@ -39,12 +51,37 @@ def manage_points_card(isJolly, cardPoints, totPoints):
     elif isJolly == 9:                                      #checks if it is the jolly
         jolly_value = 7                                     #and gives a value of 7
         sleep(0.5)
-        print("You drawn the jolly")
+        if dealer_turn == False:
+            print("You drawn the jolly")
+        else:
+            print("The dealer drawn the jolly")
     else:
         points = cardPoints                                 #else if it is a card smaller or equal to 7 it gives its nominal value
         totPoints += points
         jolly_value = 0
+
     return jolly_value, totPoints
+
+def check_win(dealerPoints, playerPoints, playerCredits, dealerStats, playerStats, playerBid):
+    if dealerPoints >= playerPoints and dealerPoints <= 7.5:
+        print("The dealer won")
+        dealerStats += 1
+        playerCredits -= playerBid
+        print("Dealer:", dealerStats, "You:", playerStats, "Now you have", playerCredits, "credits")
+            
+    elif dealerPoints < playerPoints and dealerPoints <= 7.5:
+        print("You won!")
+        playerStats += 1
+        playerCredits += playerBid
+        print("Dealer:", dealerStats, "You:", playerStats, "Now you have", playerCredits, "credits")
+            
+    elif dealerPoints > 7.5:
+        print("You won!")
+        playerStats += 1
+        playerCredits += playerBid
+        print("Dealer:", dealerStats, "You:", playerStats, "Now you have", playerCredits, "credits")
+
+    return dealerPoints, playerPoints, playerCredits, dealerStats, playerStats
 
 
 
@@ -57,14 +94,16 @@ while credits > 0:
     jolly_value = 0
     points = 0
     total_points = 0
-    puntata = 1000000000000000
+    bid = 1000000000000000
+
     sleep(0.5)
-    while puntata > credits:
-        print("You have", credits, "credits")
+
+    while bid > credits:
+        print("You have", credits, "credits")                               #it prints your total credits
         sleep(0.5)
-        puntata = int(input("How much do you want to bid? "))
-        if puntata > credits:
-            print("You can't bid more credits than the ones you have")
+        bid = int(input("How much do you want to bid? "))               #ask how much credits you want to bid
+        if bid > credits:                                               #if the bid is greater than the total credits 
+            print("You can't bid more credits than the ones you have")     
     
     
     
@@ -72,6 +111,8 @@ while credits > 0:
     print("Shuffle the deck...")
     sleep(2)
 
+    #PLAYER TURN
+    dealer_turn = False
     CardValue, CardID = draw_card()
     jolly_value, total_points = manage_points_card(CardID, CardValue, total_points)
     total_points = manage_jolly(jolly_value, total_points)
@@ -89,100 +130,58 @@ while credits > 0:
     
         sleep(0.5)
         
-        if total_points > 7.5 :
-            print("You busted")
-            credits = credits - puntata
+        if total_points > 7.5 :                                         #if the total points are > 7.5
+            print("You busted")                                         #the program prints that you busted
+            print("The dealer won")
+            dealer = dealer + 1
+            credits = credits - bid
+            print("Dealer:", dealer, "You:", player, "Now you have", credits, "credits")                                     #and remove the lost credits
             break
         elif total_points == 7.5:
             keep_or_card = "keep"
-        if total_points <= 7.5:
+        if total_points < 7.5:
             keep_or_card = input("Keep or card? (keep/card) ")
     if total_points <= 7.5:
         print("You keep with", total_points)
         sleep(0.5)
     
-        number_drawn = number_of_cards.pop()                                                                #banco
-        card_suit = number_drawn // 10
-        card_value = number_drawn % 10
-        real_value = card_value + 1
-        print("The dealer has", value[card_value], "of", cards[card_suit])
-        points = 0
-        m = 0
-        if real_value > 7 and number_drawn != 9:
-            points = 0.5
-        elif number_drawn == 9:
-            m = 7
-        else:
-            points = real_value
-        d = points + m
-        if d >= total_points:
-            keep_or_card = "keep"
-        elif d < total_points:
-            keep_or_card = "card"
+    #DEALER TURN
+        dealer_turn = True
+        total_dealer_points = 0
+        CardValue, CardID = draw_card()
+        jolly_value, total_dealer_points = manage_points_card(CardID, CardValue, total_dealer_points)
+        total_dealer_points = manage_jolly(jolly_value, total_dealer_points)
 
-        
-        
+        if total_dealer_points >= total_points:
+            keep_or_card = "keep"
+        else:
+            keep_or_card = "card"
+         
         while keep_or_card == "card":
             sleep(1)
             print("The dealer draws another card")
             sleep(1)
-            number_drawn = number_of_cards.pop()                                                                
-            card_suit = number_drawn // 10
-            card_value = number_drawn % 10
-            real_value = card_value + 1
-            print("Card:", value[card_value], "of", cards[card_suit])
-            if real_value > 7 and number_drawn != 9:
-                points = 0.5
-            elif number_drawn == 9:
-                if d % 1 == 0:
-                    m = 7 - d
-                elif d % 1 != 0:
-                    m = 7.5 - d
-            elif real_value <= 7:
-                points = real_value
-            if m != 0:
-                if d % 1 == 0:
-                    m = 7 - d
-                elif d % 1 != 0:
-                    m = 7.5 - d
-            d = d + points + m
+            
+            CardValue, CardID = draw_card()
+            jolly_value, total_dealer_points = manage_points_card(CardID, CardValue, total_dealer_points)
+            total_dealer_points = manage_jolly(jolly_value, total_dealer_points)
+
             sleep(1)
-            print("The dealer has", d)
-            if d >= total_points:
+
+            if total_dealer_points >= total_points:
                 keep_or_card = "keep"
-            elif d < total_points:
+            else:
                 keep_or_card = "card"
-            if d > 7.5:
+            if total_dealer_points > 7.5:
                 sleep(1)
                 print("The dealer busted")
                 break
             
         sleep(1)
             
-        if d >= total_points and d <= 7.5:
-            print("The dealer won")
-            dealer = dealer + 1
-            credits = credits - puntata
-            print("Dealer:", dealer, "You:", player, "Now you have", credits, "credits")
-            
-        elif total_points > 7.5:
-            print("The dealer won")
-            dealer = dealer + 1
-            credits = credits - puntata
-            print("Banco:", dealer, "Tu:", player, "Now you have", credits, "credits")
-            
-        elif d < total_points and d <= 7.5:
-            print("You won!")
-            player = player + 1
-            credits = credits + puntata
-            print("Dealer:", dealer, "You:", player, "Ora hai", credits, "crediti")
-            
-        elif d > 7.5:
-            print("You won!")
-            player = player + 1
-            credits = credits + puntata
-            print("Dealer:", dealer, "You:", player, "Now you have", credits, "credits")
 
+        total_dealer_points, total_points, credits, dealer, player = check_win(total_dealer_points, total_points, credits, dealer, player, bid)
+        
         sleep(1)
 
 print("You finished all your credits")
